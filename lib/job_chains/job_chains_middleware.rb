7784a1436776ae24b04f588165325772d13e3437
Shaheen Mojtabai <shaheen@shaheenm-mbp15.telenav.com>
2014-09-26 17:15:48 -0700

    Job chains correctly uses honey badger
    - https://thinkneardev.atlassian.net/browse/ES-473

--- lib/job_chains/job_chains_middleware.rb
+++ lib/job_chains/job_chains_middleware.rb
@@ -19,8 +19,8 @@ class JobChainsMiddleware
     return true unless worker.respond_to? :before
     return true if msg['skip_before'].to_s.downcase == 'true'
 
-    args = msg['args'] || []
-    if before_passed?(worker, args)
+
+    if before_passed?(worker, msg)
       return true
     else
       max_retries = msg['retry'].try(:to_i) || DEFAULT_MAX_RETRY_ATTEMPTS
@@ -32,7 +32,7 @@ class JobChainsMiddleware
       if last_try
         worker.respond_to?(:before_failed) ? worker.before_failed : raise("Attempted #{worker.class}, but preconditions were never met!")
       else
-        error_message = "Pre-conditions for #{worker.class}(#{args.join(',')}) failed."
+        error_message = "Pre-conditions for #{worker.class}(#{msg['args'].join(',')}) failed."
         Rails.logger.info(error_message)
         # Will cause Honeybadger to ignore the error, but Sidekiq will retry the task
         raise SilentSidekiqError.new(error_message)
@@ -45,29 +45,28 @@ class JobChainsMiddleware
   def check_postconditions(worker, msg)
     return unless worker.respond_to? :after
     return if msg['skip_after'].to_s.downcase == 'true'
-    
-    args = msg['args'] || []
+
     max_retries = msg['retry'].try(:to_i) || DEFAULT_MAX_RETRY_ATTEMPTS
     attempts = 1
-    attempts += 1 until attempts > max_retries || after_passed?(worker, args)
+    attempts += 1 until attempts > max_retries || after_passed?(worker, msg)
     if attempts > max_retries
       error_message = "Finished #{worker.class}, but postconditions failed!"
-      Honeybadger.notify(error_class: worker.class, error_message: error_message, parameters: {:args => args})
+      Honeybadger.notify_or_ignore(error_class: worker.class, error_message: error_message, parameters: msg)
     end
   end
   
   private
-  def before_passed?(worker, args)
-    worker.before(*args)
+  def before_passed?(worker, msg)
+    worker.before(*msg['args'])
   rescue => e
-    Honeybadger.notify(error_class: worker.class, error_message: "Before hook threw error: #{e.message}", parameters: {:args => args })
+    Honeybadger.notify_or_ignore(error_class: worker.class, error_message: "Before hook threw error: #{e.message}", parameters: msg)
     false
   end
 
-  def after_passed?(worker, args)
-    worker.after(*args)
-  rescue
-    Honeybadger.notify(error_class: worker.class, error_message: "After hook threw error: #{e.message}", parameters: {:args => args })
+  def after_passed?(worker, msg)
+    worker.after(*msg['args'])
+  rescue => e
+    Honeybadger.notify_or_ignore(error_class: worker.class, error_message: "After hook threw error: #{e.message}", parameters: msg)
     false
   end
 end
